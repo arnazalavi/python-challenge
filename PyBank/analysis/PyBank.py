082bb3bbbfc65802f86094676aa1f77ef121afc1
arnazalavi <arnazalavi@gmail.com>
2021-07-11 23:25:02 -0500

    python-challenge - PyBank , PyPoll

---
+++ PyBank/analysis/PyBank.py
@@ -0,0 +1,109 @@
+
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+Month_List=[]
+Profit_Losses_List=[]
+Change_Profit_Losses_List=[]
+Total_number_months = 0
+file_index = 1
+row_count=0
+sum_profit_loss = 0
+G_increase_profit = 0
+G_increase_profit_index = 0
+G_increase_profit_Month = 0
+G_Decrease_profit_index = 0
+G_Decrease_profit_Month = 0
+
+G_decrease_profit = -1
+Profit_Losses = 0
+Curr_Profit_Losses = 0
+Prev_Profit_Losses = None
+Prev_Profit_Losses_value = 0
+Curr_Profit_Losses_value = 0
+Change_Profit_Losses_value = 0
+diff_sum_profit_loss = 0
+Average_Change = 0
+i = 0
+
+def G_increase_profit_Month_check():
+    return Month_List[Change_Profit_Losses_List.index(max(Change_Profit_Losses_List)) +1]
+
+def G_decrease_profit_Month_check():
+    return Month_List[Change_Profit_Losses_List.index(min(Change_Profit_Losses_List)) +1]
+
+#csvpath = os.path.join('PyBank_Resources_budget_data.csv')
+csvpath = os.path.join("..","Resources","budget_data.csv")
+
+
+with open(csvpath,"r") as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+
+    for row  in csvreader:
+        Month_Value=row[0]
+        Profit_Losses_Value=row[1]
+        #Create Month and profit losses list
+        Month_List.append(Month_Value)
+        Profit_Losses_List.append(int(Profit_Losses_Value))
+
+Total_number_months = len(Month_List)
+
+print(f"Fanancials Analysis")
+print(f"-----------------")
+
+print(f"Total Months:{Total_number_months}")
+
+sum_profit_loss = 0
+print(f"Total Profit/loss : ${sum(Profit_Losses_List)}")
+
+for value in Profit_Losses_List:
+    # First comparision
+    if Prev_Profit_Losses == None:
+         Prev_Profit_Losses = "NotNone"
+         Prev_Profit_Losses_value = value
+         
+    elif Prev_Profit_Losses is not None:
+        
+        Curr_Profit_Losses_value = value
+        Change_Profit_Losses_value = int(Curr_Profit_Losses_value) - int(Prev_Profit_Losses_value)
+        Prev_Profit_Losses_value = value
+        Change_Profit_Losses_List.append(Change_Profit_Losses_value)
+     
+
+diff_sum_profit_loss = 0
+i = 0
+for i in range(len(Change_Profit_Losses_List)):
+    
+    diff_sum_profit_loss = diff_sum_profit_loss + Change_Profit_Losses_List[i]
+   
+print(f" Change total amount of Profit/Losses over the entire period: $({diff_sum_profit_loss})")
+Average_Change = round(diff_sum_profit_loss/len(Change_Profit_Losses_List),2)
+
+print(f" Average Change is ${Average_Change}")
+G_increase_profit_Month = G_increase_profit_Month_check()
+G_Decrease_profit_Month = G_decrease_profit_Month_check()
+print(f"Greatest Increase in Profits: {G_increase_profit_Month} (${max(Change_Profit_Losses_List)}) ")
+print(f"Greatest Decrease in Profits: {G_Decrease_profit_Month} (${min(Change_Profit_Losses_List)}) ")
+# write to an out put file
+with open("Financial Analysis.txt", 'w', newline='') as output:
+
+    # Initialize csv.writer
+    #csvwriter = csv.writer(csvfile, delimiter=',')
+    print(f"Financial Analysis", file = output)
+    print(f"-----------------", file=output)
+    print(f"Total Months:{Total_number_months}", file=output)
+    print(f"Total Profit/loss : ${sum(Profit_Losses_List)}", file=output)
+
+       
+    print(f" Change total amount of Profit/Losses over the entire period: $({diff_sum_profit_loss})", file=output)
+
+    print(f" Average Change is ${Average_Change}", file=output)
+    print(f"Greatest Increase in Profits: {G_increase_profit_Month} (${max(Change_Profit_Losses_List)}) ", file=output)
+    print(f"Greatest Decrease in Profits: {G_Decrease_profit_Month} (${min(Change_Profit_Losses_List)}) " ,file=output)
---
+++ PyPoll/analysis/PyPoll.py
@@ -0,0 +1,83 @@
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+Total_Votes = 0
+candidates_list =[]
+#candidates_votes = [0]*len(candidates_list)
+#candidates_votes = [0]*4
+candidates_votes = []
+candidates_list_index = 0
+candidates_votes_percent = 0
+
+#Create a candidate list and Candidate vote list
+def candidates_list_check(candidate_name):
+            # if candidates not in candidates list then add to a list
+            if (candidate_name not in candidates_list):
+                
+                candidates_list.append(candidate_name)
+                candidates_index = candidates_list.index(candidate_name)
+                candidates_votes.append(0)
+
+# function return the winner of the election
+def election_winner():
+    return candidates_list[candidates_votes.index(max(candidates_votes))]
+
+csvpath = os.path.join("..","Resources","election_data.csv")
+with open(csvpath,"r") as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+# Go through each row in the file 
+#get the index from the candidates list using name as index
+# Add the candidates votes to a candidates votes list 
+    for row  in csvreader:
+        candidates_list_check(row[2])
+        candidates_list_index = candidates_list.index(row[2])
+        candidates_votes[candidates_list_index] = candidates_votes[candidates_list_index] +1 
+    
+    Total_Votes = sum(candidates_votes)
+
+    print(f"Election Result")
+    print(f"-----------------")
+    print(f" Total Votes:{Total_Votes}")
+    print(f"-----------------")
+    
+    for i in  range(len(candidates_list)):
+        # get the index of a candidate from candidate list
+        # use the index to go to candidates_votes list and get the votes
+        #print(candidates_list[i])
+        candidates_list_index = candidates_list.index(candidates_list[i])
+        candidates_votes_percent = round(((candidates_votes[candidates_list_index] / Total_Votes) * 100),2)
+        #candidates_votes_percent = candidates_votes[candidates_list_index] / Total_Votes * 100
+        print(f"{candidates_list[i]}: {candidates_votes_percent}%  ({candidates_votes[candidates_list_index]})")
+ 
+Election_Winner =election_winner() 
+print(f"-----------------")
+print(f"Election Winner : {Election_Winner}")
+print(f"-----------------")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open("Election_Result.txt", 'w', newline='') as output:
+
+    # Initialize csv.writer
+    #csvwriter = csv.writer(csvfile, delimiter=',')
+    print(f"Election Result", file = output)
+    print(f"-----------------", file = output)
+    print(f" Total Votes:{Total_Votes}", file = output)
+    print(f"-----------------", file = output)
+    for i in  range(len(candidates_list)):
+        # get the index of a candidate from candidate list
+        # use the index to go to candidates_votes list and get the votes
+        #print(candidates_list[i])
+        candidates_list_index = candidates_list.index(candidates_list[i])
+        candidates_votes_percent = round(((candidates_votes[candidates_list_index] / Total_Votes) * 100),2)
+        #print(candidates_votes_percent)
+        #candidates_votes_percent = candidates_votes[candidates_list_index] / Total_Votes * 100
+        print(f"{candidates_list[i]}: {candidates_votes_percent}%  ({candidates_votes[candidates_list_index]})", file=output)
+    print(f"-----------------", file = output)
+    print(f"Election Winner : {Election_Winner}",file = output)
+    print(f"-----------------", file = output)
